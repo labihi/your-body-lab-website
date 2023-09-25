d1072c4270200cfc10511d26b2c4817df045cfea
Lorenzo "labihi" Garibaldi <lorenzo.garibaldi@itj.it>
2023-09-25 20:01:27 +0200

    edited the template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,14 @@ module.exports = {
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+      backgroundColor: {
+        primary: "#71db06",
+        secondary: "#ffa403",
+      },
+      textColor: {
+        primary: "#71db06",
+        secondary: "#ffa403",
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
